077a50567b76cb026d0665a9a38c4af29f82875a
A247ZG <joseph.arul@axa.fr>
2025-12-04 16:26:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# SeleniumJunit5
+An example of using Selenium WebDriver and JUnit 5
+
+# create a new repository on the command line
+git init
+
+git add README.md 
+
+git commit -m "first commit" 
+
+git branch -M main 
+
+git remote add origin https://github.com/josepharul82/CopilotForAutomation.git 
+
+git push -u origin main 
+
+# push an existing repository from the command line
+
+git remote add origin https://github.com/josepharul82/CopilotForAutomation.git 
+
+git branch -M main 
+
+git push -u origin main 
+
+
+
---
+++ src/test/java/fr/axa/automation/sample/SampleTest.java
@@ -0,0 +1,137 @@
+package fr.axa.automation.sample;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
+
+public class SampleTest {
+	private WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        WebDriverManager.edgedriver().setup();
+        webDriver =  new EdgeDriver();
+    }
+
+    @AfterEach
+    public void afterScenario()  throws Exception {
+        webDriver.quit();
+    }
+
+	@Test
+	public void testEndToEndHomeInsurance() throws InterruptedException {
+		openApplication();
+        testLoginPage();
+        testResearchPage();
+        fillHomeDetail();
+	}
+
+    private void openApplication() {
+        webDriver.get("https://axafrance.github.io/webengine-dotnet/demo/home-insurance/");
+        webDriver.manage().window().maximize();
+    }
+
+    private void testLoginPage() {
+        fillLogin();
+        fillPassword();
+        clickNextButton();
+    }
+
+    private void fillLogin() {
+        WebElement loginWebElement = waitForVisibility(webDriver, By.name("login"), 30);
+        loginWebElement.sendKeys("login");
+    }
+
+    private void fillPassword() {
+        WebElement passwordWebElement = waitForVisibility(webDriver, By.name("password"), 30);
+        passwordWebElement.sendKeys("test");
+    }
+
+    private void clickNextButton() {
+        WebElement nextButton = waitForVisibility(webDriver, By.xpath("/html/body/div/div[4]/button[2]"), 30);
+        nextButton.click();
+    }
+
+    private void testResearchPage() {
+    }
+
+    private void fillHomeDetail() {
+    }
+
+
+    public static WebElement waitForVisibility(WebDriver driver, By locator, int timeoutInSeconds) {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))  // Maximum time to wait
+                .pollingEvery(Duration.ofMillis(500)) // Interval between each poll
+                .ignoring(NoSuchElementException.class); // Exceptions to ignore
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static WebElement waitForElementClickable(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebElement webElement = waitForVisibility( driver, locator, timeoutInSeconds);
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))  // Maximum time to wait
+                .pollingEvery(Duration.ofMillis(500)) // Interval between each poll
+                .ignoring(ElementClickInterceptedException.class); // Exceptions to ignore
+        return wait.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    public static void waitUntilEndOfPage(WebDriver driver, int timeoutInSeconds) {
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeoutInSeconds))
+                .pollingEvery(Duration.ofMillis(500))
+                .until(d -> {
+                    JavascriptExecutor js = (JavascriptExecutor) d;
+                    Long scrollY = (Long) js.executeScript("return window.scrollY + window.innerHeight;");
+                    Long pageHeight = (Long) js.executeScript("return document.body.scrollHeight;");
+                    return scrollY >= pageHeight;
+                });
+    }
+
+    public static void waitUntilElementHasFocus(WebDriver driver, WebElement element, int timeoutInSeconds) {
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeoutInSeconds))
+                .pollingEvery(Duration.ofMillis(500))
+                .until(d -> {
+                    JavascriptExecutor js = (JavascriptExecutor) d;
+                    return (Boolean) js.executeScript("return arguments[0] === document.activeElement;", element);
+                });
+    }
+
+    // Fait défiler la page jusqu’à l’élément donné
+    public static void scrollToElement(WebDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
+    }
+
+    // Attends que l’élément soit dans la fenêtre après avoir scrollé
+    public static void waitUntilElementInView(WebDriver driver, WebElement element, int timeoutInSeconds) {
+        scrollToElement(driver, element);
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeoutInSeconds))
+                .pollingEvery(Duration.ofMillis(500))
+                .until(d -> {
+                    JavascriptExecutor js = (JavascriptExecutor) d;
+                    return (Boolean) js.executeScript(
+                            "const rect = arguments[0].getBoundingClientRect();" +
+                                    "return rect.top >= 0 && rect.bottom <= window.innerHeight;", element);
+                });
+    }
+
+
+}
